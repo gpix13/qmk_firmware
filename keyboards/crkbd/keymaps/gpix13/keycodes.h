c4a100127fa130c1afce5ca78a16f8ea4d9f2fd0
gpix13 <gpix13@gmail.com>
2023-12-07 09:43:34 -0600

    update to latest

--- keyboards/crkbd/keymaps/gpix13/keycodes.h
+++ keyboards/crkbd/keymaps/gpix13/keycodes.h
@@ -15,15 +15,24 @@ enum layers {
 
 // home row mods
 #define HR_A   LSFT_T(KC_A)
-#define HR_S   LGUI_T(KC_S)
+#define HR_S   LCTL_T(KC_S)
 #define HR_D   LALT_T(KC_D)
-#define HR_F   LCTL_T(KC_F)
-#define HR_J   LCTL_T(KC_J)
+#define HR_F   LGUI_T(KC_F)
+#define HR_J   LGUI_T(KC_J)
 #define HR_K   LALT_T(KC_K)
-#define HR_L   LGUI_T(KC_L)
+#define HR_L   LCTL_T(KC_L)
 #define HR_SC  LSFT_T(KC_SCLN)
+
+// Tap Dance
+// ---------------------------
+// tap: Q, hold: COPY
+#define TD_Q   TD(DANCE_1)
+// tap: W, hold: PASTE
+#define TD_W   TD(DANCE_2)
 
 // shortcuts
 #define HOME     G(KC_LEFT)
 #define END      G(KC_RIGHT)
+#define COPY     G(KC_C)
+#define PASTE    G(KC_V)
 
--- keyboards/crkbd/keymaps/gpix13/keymap.c
+++ keyboards/crkbd/keymaps/gpix13/keymap.c
@@ -2,11 +2,18 @@
 #include QMK_KEYBOARD_H
 #include "keycodes.h"
 
+// Tap Dance definitions
+enum {
+    DANCE_1,
+    DANCE_2,
+};
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      XXXXXXX,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, XXXXXXX,
+      XXXXXXX,    TD_Q,    TD_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       XXXXXXX,    HR_A,    HR_S,    HR_D,    HR_F,    KC_G,                         KC_H,    HR_J,    HR_K,    HR_L,   HR_SC, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -54,7 +61,153 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 };
 
+// Enable ADJUST layer when holding down LOWER and RAISE keys
 layer_state_t layer_state_set_user(layer_state_t state) {
    return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 }
+
+// Set tapping term for specific keys
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case BSP_RSE:
+        case SFT_ENT:
+        case LOW_TAB:
+            return 150;
+        case HR_A:
+        case HR_S:
+            return TAPPING_TERM + 50;
+        default:
+            return TAPPING_TERM;
+    }
+}
+
+// -----------------------------------------------------------------------------
+//                               TAP DANCE
+// -----------------------------------------------------------------------------
+
+extern bool g_suspend_state;
+#define GET_TAP_KC(dual_role_key) dual_role_key & 0xFF
+uint16_t last_keycode = KC_NO;
+uint8_t last_modifier = 0;
+
+typedef struct {
+    bool is_press_action;
+    uint8_t step;
+} tap;
+
+enum {
+    SINGLE_TAP = 1,
+    SINGLE_HOLD,
+    DOUBLE_TAP,
+    DOUBLE_HOLD,
+    DOUBLE_SINGLE_TAP,
+    MORE_TAPS
+};
+
+static tap dance_state = {
+    .is_press_action = true,
+    .step = 0
+};
+
+void on_dance_1(tap_dance_state_t *state, void *user_data);
+uint8_t dance_1_dance_step(tap_dance_state_t *state);
+void dance_1_finished(tap_dance_state_t *state, void *user_data);
+void dance_1_reset(tap_dance_state_t *state, void *user_data);
+
+void on_dance_1(tap_dance_state_t *state, void *user_data) {
+	if(state->count == 3) {
+		tap_code16(KC_Q);
+		tap_code16(KC_Q);
+		tap_code16(KC_Q);
+	}
+	if(state->count > 3) {
+		tap_code16(KC_Q);
+	}
+}
+
+uint8_t dance_1_dance_step(tap_dance_state_t *state) {
+	if (state->count == 1) {
+		if (state->interrupted || !state->pressed) return SINGLE_TAP;
+		else return SINGLE_HOLD;
+	} else if (state->count == 2) {
+		if (state->interrupted) return DOUBLE_SINGLE_TAP;
+		else if (state->pressed) return DOUBLE_HOLD;
+		else return DOUBLE_TAP;
+	}
+	return MORE_TAPS;
+}
+void dance_1_finished(tap_dance_state_t *state, void *user_data) {
+	dance_state.step = dance_1_dance_step(state);
+	switch (dance_state.step) {
+		case SINGLE_TAP: register_code16(KC_Q); break;
+		case SINGLE_HOLD: register_code16(COPY); break;
+		case DOUBLE_TAP: register_code16(KC_Q); register_code16(KC_Q); break;
+		case DOUBLE_SINGLE_TAP: tap_code16(KC_Q); register_code16(KC_Q);
+	}
+}
+
+void dance_1_reset(tap_dance_state_t *state, void *user_data) {
+	wait_ms(10);
+	switch (dance_state.step) {
+		case SINGLE_TAP: unregister_code16(KC_Q); break;
+		case SINGLE_HOLD: unregister_code16(COPY); break;
+		case DOUBLE_TAP: unregister_code16(KC_Q); break;
+		case DOUBLE_SINGLE_TAP: unregister_code16(PASTE); break;
+	}
+	dance_state.step = 0;
+}
+
+void on_dance_2(tap_dance_state_t *state, void *user_data);
+uint8_t dance_2_dance_step(tap_dance_state_t *state);
+void dance_2_finished(tap_dance_state_t *state, void *user_data);
+void dance_2_reset(tap_dance_state_t *state, void *user_data);
+
+void on_dance_2(tap_dance_state_t *state, void *user_data) {
+	if(state->count == 3) {
+		tap_code16(KC_W);
+		tap_code16(KC_W);
+		tap_code16(KC_W);
+	}
+	if(state->count > 3) {
+		tap_code16(KC_W);
+	}
+}
+
+uint8_t dance_2_dance_step(tap_dance_state_t *state) {
+	if (state->count == 1) {
+		if (state->interrupted || !state->pressed) return SINGLE_TAP;
+		else return SINGLE_HOLD;
+	} else if (state->count == 2) {
+		if (state->interrupted) return DOUBLE_SINGLE_TAP;
+		else if (state->pressed) return DOUBLE_HOLD;
+		else return DOUBLE_TAP;
+	}
+	return MORE_TAPS;
+}
+void dance_2_finished(tap_dance_state_t *state, void *user_data) {
+	dance_state.step = dance_2_dance_step(state);
+	switch (dance_state.step) {
+		case SINGLE_TAP: register_code16(KC_W); break;
+		case SINGLE_HOLD: register_code16(PASTE); break;
+		case DOUBLE_TAP: register_code16(KC_W); register_code16(KC_W); break;
+		case DOUBLE_SINGLE_TAP: tap_code16(KC_W); register_code16(KC_W);
+	}
+}
+
+void dance_2_reset(tap_dance_state_t *state, void *user_data) {
+	wait_ms(10);
+	switch (dance_state.step) {
+		case SINGLE_TAP: unregister_code16(KC_W); break;
+		case SINGLE_HOLD: unregister_code16(PASTE); break;
+		case DOUBLE_TAP: unregister_code16(KC_W); break;
+		case DOUBLE_SINGLE_TAP: unregister_code16(PASTE); break;
+	}
+	dance_state.step = 0;
+}
+
+
+tap_dance_action_t tap_dance_actions[] = {
+    [DANCE_1] = ACTION_TAP_DANCE_FN_ADVANCED(on_dance_1, dance_1_finished, dance_1_reset),
+    [DANCE_2] = ACTION_TAP_DANCE_FN_ADVANCED(on_dance_2, dance_2_finished, dance_2_reset),
+};
 
