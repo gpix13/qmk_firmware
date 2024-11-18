0d6f9ffef69da32752e4a03458f87c397615c5c1
gpix13 <gpix13@gmail.com>
2024-11-18 13:45:57 -0600

    adjust timing for thumb layer-tap keys

--- keyboards/ferris/keymaps/gpix13/keymap.c
+++ keyboards/ferris/keymaps/gpix13/keymap.c
@@ -45,7 +45,7 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case BSP_RSE:
         case LOW_TAB:
-            return 150;
+            return 140;
         default:
             return TAPPING_TERM;
     }
